9faa1c3d6f00c26c3943f81d0fd3d3c4f01a02d2
ElianaDaniel14 <danieleliana35@gmail.com>
2025-06-14 03:41:50 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,9 @@
+$(function() {
+    
+        $("#image-service").fadeIn(5000).fadeOut(5000).fadeIn(5000);
+        $("#image-service2").fadeIn(10000).fadeOut(10000).fadeIn(10000);
+        $("#image-service3").fadeIn(15000).fadeOut(15000).fadeIn(15000);
+
+
+
+});
